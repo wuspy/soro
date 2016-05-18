e7ab0f7aea4d3c372b99b0ea2cb408aa90978a6d
DoctaSnowMan <doublejinitials@gmail.com>
2016-05-18 15:54:16 -0500

    Make Rover2 actually work

--- Rover2/main.cpp
+++ Rover2/main.cpp
@@ -1,8 +1,14 @@
 #include <QCoreApplication>
 
+#include "rover2process.h"
+
+using namespace Soro::Rover;
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    Rover2Process worker(&a);
 
     return a.exec();
 }
