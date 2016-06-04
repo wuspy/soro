850627c03c9458afc39739ba5341712bf8023757
DoctaSnowMan <doublejinitials@gmail.com>
2016-06-03 22:18:35 -0500

    Remove the 'Bill Regex'

--- MissionControl/setupdialog.cpp
+++ MissionControl/setupdialog.cpp
@@ -31,28 +31,6 @@ bool SetupDialog::verifyName() {
         QMessageBox::critical(this, "Error", "Enter a name for your mission control", QMessageBox::Ok);
         return false;
     }
-    QRegularExpression antiBill("(bu(s|\\$)h.|d(i|1)ck.cheney)"
-                                "*("
-                                    "(made.*m(o|0)ney)"
-                                    "|iraq"
-                                    "|((9|n(i|1)ne)(-|\\/| )?(11|e(1|l)even))"
-                                    "| wmd"
-                                    "|weap(o|0)n.*mass destruct(i|1)(o|0)n"
-                                    "|jet *fuel"
-                                ")|inside.*j(o|0)b"
-                                "|(george *(w( |\\.|$)|bu(s|\\$)h))"
-                                "|((jet *fuel|melt).*(stee(l|1)|beams))"
-                                "|((thermite|b(o|0)mb).*gr(o|0)und *(zer(o|0)|0))"
-                                "|terr(o|0)r(i|1)st",
-                                QRegularExpression::CaseInsensitiveOption);
-
-    QRegularExpressionMatch match = antiBill.match(ui->nameLineEdit->text());
-    if (match.hasMatch()) {
-        QMessageBox::information(this, "Error", "Fuck off Bill", QMessageBox::Ok);
-        return false;
-    }
-
-    _name = ui->nameLineEdit->text().trimmed();
     return true;
 }
 
